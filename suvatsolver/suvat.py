d6032b160643a06be16b4ac345516e1157c09ee1
Jonathan Rudman <jonathan.rudman@live.co.uk>
2023-09-06 10:30:10 +0100

    Support uncertain values

--- suvatsolver/suvat.py
+++ suvatsolver/suvat.py
@@ -2,16 +2,17 @@ import math
 from pydantic import validate_call
 from dataclasses import dataclass, asdict
 from typing import Self
+import itertools
 
 
 @validate_call
 @dataclass
 class Suvat:
-    s: float | None = None
-    u: float | None = None
-    v: float | None = None
-    a: float | None = None
-    t: float | None = None
+    s: list[float] | None = None
+    u: list[float] | None = None
+    v: list[float] | None = None
+    a: list[float] | None = None
+    t: list[float] | None = None
 
     def unknowns(self) -> list[str]:
         return [key for key, val in asdict(self).items() if val is None]
@@ -29,27 +30,39 @@ class Suvat:
             return self
 
         if self.t == 0:
-            self.s = 0
+            self.s = [0.0]
             return self
 
         # Given u, a, t
         if self.u is not None and self.a is not None and self.t is not None:
-            self.s = (self.u * self.t) + (0.5 * self.a * (self.t**2))
+            self.s = [
+                (u * t) + (0.5 * a * (t**2))
+                for u, a, t in itertools.product(self.u, self.a, self.t)
+            ]
             return self
 
         # Given u, v, t
         if self.u is not None and self.v is not None and self.t is not None:
-            self.s = 0.5 * self.t * (self.u + self.v)
+            self.s = [
+                0.5 * t * (u + v)
+                for u, v, t in itertools.product(self.u, self.v, self.t)
+            ]
             return self
 
         # Given v, a, t
         if self.v is not None and self.a is not None and self.t is not None:
-            self.s = (self.v * self.t) - (0.5 * self.a * (self.t**2))
+            self.s = [
+                (v * t) - (0.5 * a * (t**2))
+                for v, a, t in itertools.product(self.v, self.a, self.t)
+            ]
             return self
 
         # Given u, v, a
         if self.u is not None and self.v is not None and self.a is not None:
-            self.s = ((self.v**2) - (self.u**2)) / (2.0 * self.a)
+            self.s = [
+                ((v**2) - (u**2)) / (2.0 * a)
+                for u, v, a in itertools.product(self.u, self.v, self.a)
+            ]
             return self
 
         raise Exception("Cannot be solved with more than two unknowns!")
@@ -59,27 +72,40 @@ class Suvat:
             return self
 
         if self.t == 0:
-            self.u = 0
+            self.u = [0.0]
             return self
 
         # Given v, a, t
         if self.v is not None and self.a is not None and self.t is not None:
-            self.u = self.v - (self.a * self.t)
+            self.u = [
+                v - (a * t) for v, a, t in itertools.product(self.v, self.a, self.t)
+            ]
             return self
 
         # Given s, v, t
         if self.s is not None and self.v is not None and self.t is not None:
-            self.u = ((2.0 * self.s) / self.t) - self.v
+            self.u = [
+                ((2.0 * s) / t) - v
+                for s, v, t in itertools.product(self.s, self.v, self.t)
+            ]
             return self
 
-        # Given s, v, a
+        # Given s, v, a, no certainty when dealing with negatives
         if self.s is not None and self.v is not None and self.a is not None:
-            self.u = math.sqrt((self.v**2) - 2.0 * self.a * self.s)
+            self.u = []
+            for s, v, a in itertools.product(self.s, self.v, self.a):
+                self.u += [
+                    math.sqrt((v**2) - 2.0 * a * s),
+                    -1 * math.sqrt((v**2) - 2.0 * a * s),
+                ]
             return self
 
         # Given s, a, t
         if self.s is not None and self.a is not None and self.t is not None:
-            self.u = (self.s / self.t) - 0.5 * self.a * self.t
+            self.u = [
+                (s / t) - 0.5 * a * t
+                for s, a, t in itertools.product(self.s, self.a, self.t)
+            ]
             return self
 
         raise Exception("Cannot be solved with more than two unknowns!")
@@ -89,27 +115,40 @@ class Suvat:
             return self
 
         if self.t == 0:
-            self.v = 0
+            self.v = [0.0]
             return self
 
         # Given u, a, t
         if self.u is not None and self.a is not None and self.t is not None:
-            self.v = self.u + (self.a * self.t)
+            self.v = [
+                u + (a * t) for u, a, t in itertools.product(self.u, self.a, self.t)
+            ]
             return self
 
         # Given s, u, a
         if self.s is not None and self.u is not None and self.a is not None:
-            self.v = math.sqrt((self.u**2) + (2 * self.a * self.s))
+            self.v = []
+            for s, u, a in itertools.product(self.s, self.u, self.a):
+                self.v += [
+                    math.sqrt((u**2) + (2 * a * s)),
+                    -1 * math.sqrt((u**2) + (2 * a * s)),
+                ]
             return self
 
         # Given s, a, t
         if self.s is not None and self.a is not None and self.t is not None:
-            self.v = (self.s / self.t) + (0.5 * self.a * self.t)
+            self.v = [
+                (s / t) + (0.5 * a * t)
+                for s, a, t in itertools.product(self.s, self.a, self.t)
+            ]
             return self
 
         # Given s, u, t
         if self.s is not None and self.u is not None and self.t is not None:
-            self.v = ((2 * self.s) / self.t) - self.u
+            self.v = [
+                ((2 * s) / t) - u
+                for s, u, t in itertools.product(self.s, self.u, self.t)
+            ]
             return self
 
         raise Exception("Cannot be solved with more than two unknowns!")
@@ -119,27 +158,38 @@ class Suvat:
             return self
 
         if self.t == 0:
-            self.a = 0
+            self.a = [0.0]
             return self
 
         # Given u, v, t
         if self.u is not None and self.v is not None and self.t is not None:
-            self.a = (self.v - self.u) / self.t
+            self.a = [
+                (v - u) / t for u, v, t in itertools.product(self.u, self.v, self.t)
+            ]
             return self
 
         # Given s, u, v
         if self.s is not None and self.u is not None and self.v is not None:
-            self.a = ((self.v**2) - (self.u**2)) / (2.0 * self.s)
+            self.a = [
+                ((v**2) - (u**2)) / (2.0 * s)
+                for s, u, v in itertools.product(self.s, self.u, self.v)
+            ]
             return self
 
         # Given s, u, t
         if self.s is not None and self.u is not None and self.t is not None:
-            self.a = (2.0 * (self.s - (self.u * self.t))) / (self.t**2)
+            self.a = [
+                (2.0 * (s - (u * t))) / (t**2)
+                for s, u, t in itertools.product(self.s, self.u, self.t)
+            ]
             return self
 
         # Given s, v, t
         if self.s is not None and self.v is not None and self.t is not None:
-            self.a = (2.0 * ((self.v * self.t) - self.s)) / (self.t**2)
+            self.a = [
+                (2.0 * ((v * t) - s)) / (t**2)
+                for s, v, t in itertools.product(self.s, self.v, self.t)
+            ]
             return self
 
         raise Exception("Cannot be solved with more than two unknowns!")
@@ -148,31 +198,41 @@ class Suvat:
         if self.t is not None:
             return self
 
-        # Given u, v, s
+        # Given s, u, v
         if self.s is not None and self.u is not None and self.v is not None:
-            self.t = (2.0 * self.s) / (self.u + self.v)
+            self.t = [
+                (2.0 * s) / (u + v)
+                for s, u, v in itertools.product(self.s, self.u, self.v)
+            ]
             return self
 
         # Given s, u, a
         if self.s is not None and self.u is not None and self.a is not None:
-            self.t = self.u / (self.s - (0.5 * self.a))
+            self.t = [
+                u / (s - (0.5 * a))
+                for s, u, a in itertools.product(self.s, self.u, self.a)
+            ]
             return self
 
         # Given u, v, a
         if self.u is not None and self.v is not None and self.a is not None:
-            if self.v - self.u == 0 and self.a == 0:
-                self.t = 1.0
-                return self
-            self.t = (self.v - self.u) / self.a
+            self.t = []
+            for u, v, a in itertools.product(self.u, self.v, self.a):
+                if v - u == 0 and a == 0:
+                    self.t += [1.0]
+                    return self
+                self.t += [(v - u) / a]
             return self
 
-        # Given s, v, a
+        # Given s, v, a, the u generated in the formula will be positive
         if self.s is not None and self.v is not None and self.a is not None:
-            top = self.v - math.sqrt((self.v**2) - (2.0 * self.a * self.s))
-            if top == 0 and self.a == 0:
-                self.t = 1.0
-                return self
-            self.t = top / self.a
+            self.t = []
+            for s, v, a in itertools.product(self.s, self.v, self.a):
+                top = v - math.sqrt((v**2) - (2.0 * a * s))
+                if top == 0 and self.a == 0:
+                    self.t += [1.0]
+                    return self
+                self.t += [top / a, -1 * top / a]
             return self
 
         raise Exception("Cannot be solved with more than two unknowns!")
